f273a19ae3b419fcbe59bbac60f6605b304f90e0
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-12-10 17:54:37 +0500

    completed hero section

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,7 @@
-import { ClassValue, clsx } from "clsx";
+// src/lib/utils.ts  (ya lib/utils.ts)
+
+import type { ClassValue } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
