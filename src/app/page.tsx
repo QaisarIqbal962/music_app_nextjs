b53f6aeb8ad21a3af4f77b83bcd32d6e9c2fd328
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-12-11 17:47:40 +0500

    Up coming webinar section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
 import Testimonial from "@/components/Testimonial";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <FeaturedSection/>
       <WhyChooseUs/>
       <Testimonial/>
+      <UpcomingWebinars/>
     </main>
   );
 }
