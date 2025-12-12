a365de3ae8d9fdc31054ecf93c4054fa8d56627e
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-12-12 10:47:52 +0500

    start constructor section

---
+++ src/components/Instructors.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { WavyBackground } from "./ui/wavy-background";
+
+function Instructors() {
+  return (
+    <div className="text-white relative h-160 overflow-hidden flex items-center justify-center">
+        
+        <WavyBackground/>
+
+    </div>
+  );
+}
+
+export default Instructors;
